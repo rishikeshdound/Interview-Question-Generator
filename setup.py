4f7ac5871a4f28b4d022070f5537cac8c8405020
rishikeshdound <rishikeshdound@gmail.com>
2024-06-02 20:49:15 +0530

    packages installed

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    
+    name= "Genrative AI Project",
+    version= "0.0.0",
+    author= "rishikesh",
+    author_email='rishikeshdound@gmail.com',
+    
+    packages= find_packages(),
+    install_requires = []
+    
+)
